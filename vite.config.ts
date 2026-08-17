60f0d2f681b37a66f3c6fe32c721ab04ca01d174
wilfgrainger <51911955+wilfgrainger@users.noreply.github.com>
2026-08-17 21:39:47 +0100

    chore: proxy local API through Vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ export default defineConfig({
   },
   server: {
     host: '127.0.0.1',
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8787',
+        changeOrigin: false
+      }
+    }
   }
 });
