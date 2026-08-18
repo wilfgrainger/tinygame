5b0d83a080b6351beb12733ef26d613d867658f6
Wilf Grainger <wjgrainger@gmail.com>
2026-08-18 11:43:59 +0100

    asd

--- src/generated/release.ts
+++ src/generated/release.ts
@@ -1,7 +1,7 @@
 export const APP_VERSION = "0.1.0";
-export const COMMIT_SHA = "89a2f72ffa870ab0e358eb86ce2e0dc371b21462";
-export const BUILD_TIME = "2026-08-18T10:36:43.576Z";
-export const TINY_ENV = "local";
+export const COMMIT_SHA = "ab003bc6b90e0965f9a3596dc232c0a28881c7e5";
+export const BUILD_TIME = "2026-08-18T10:37:19.632Z";
+export const TINY_ENV = "dev";
 
 export type ReleaseMetadata = { version: string; commitSha: string; buildTime: string; environment: string };
 export const RELEASE: ReleaseMetadata = { version: APP_VERSION, commitSha: COMMIT_SHA, buildTime: BUILD_TIME, environment: TINY_ENV };
